76401d23ebceafb189116c3d42e77fbeef30ba6b
--add <2616718f@student.gla.ac.uk>
2024-04-01 12:14:50 +0100

    complete function

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,14 @@ use csv;
 use std::error::Error;
 
 fn read_from_file(path: &str) -> Result<(), Box<dyn Error>>{
+    let mut reader = csv::Reader::from_path(path)?;
+
+    for result in reader.records() {
+        let record = result?;
+
+        println!("{:?}", record)
+    }
+    Ok(())
 }
 
 fn main() {
