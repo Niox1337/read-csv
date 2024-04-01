0a0cb8f38b56bd285d24808d1df20b073680a1a6
--add <2616718f@student.gla.ac.uk>
2024-04-01 12:07:11 +0100

    create function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use csv;
 use std::error::Error;
 
+fn read_from_file(path: &str) -> Result<(), Box<dyn Error>>{
+}
+
 fn main() {
     if let Err(e) = read_from_file("data.csv") {
         eprintln!("Error reading file: {}", e);
